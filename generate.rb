8080591161a45a979b1538730433b0578d370992
Nelson Chen <crazysim@gmail.com>
2008-08-31 02:45:33 -0700

    stalled

--- generate.rb
+++ generate.rb
@@ -1,12 +1,15 @@
 #!/usr/bin/env ruby
+# This is shit code, don't use it.
 
 require 'rubygems'
 require 'hpricot'
+require 'open-uri'
 require 'gruff'
 
 # Example Link
 # http://www.imdb.com/title/tt0804503/eprate
 # doc.search("//tr/td[@bgcolor=#eeeeee]").last.parent
+# doc.search("//h1").first.children.first.inner_text
 
 # Ask for Link (http://www.imdb.com/title/tt0804503/eprate)
 # Give Image File (tt0804503.png)
@@ -20,5 +23,56 @@ require 'gruff'
 # Create Dataset
 # Generate Graph
 
+class Show
+  attr_accessor :name
+  attr_accessor :seasons
+  def initialize(url)
+    url.sub!("rate","date")
+    html = open(url)
+    @doc = Hpricot(html)
+    @seasons = {}
+    process
+  end
+  
+  
+  def save_png(file)
+    generate_graph
+  end
+  
+  private
+  def process
+    
+    season_number = doc.search("//tr/td[@bgcolor=#eeeeee]").last.inner_text.to_i
+    
+    doc.search("//tr/td[@bgcolor=#eeeeee]").each do |e|
+      e = e.parent
+      puts e
+    end
+  end
+end
+  
+class Season
+  attr_accessor :length
+  attr_accessor :shows
+  
+  def initialize
+    @shows = []
+  end
+  
+  def avg_rating
+    total_rating = 0
+    @shows.each do |s|
+      s.rating += rating
+    end
+    total_rating/@shows.length
+  end
+end
 
-
+class Episode
+  attr_accessor :pc
+  attr_accessor :epnumber
+  attr_accessor :season
+  attr_accessor :name
+  attr_accessor :rating
+end
+url = "http://www.imdb.com/title/tt0804503/eprate"

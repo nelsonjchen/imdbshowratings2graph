d2c0f15364f752275c28d912beeb3a1a6e6172c0
Nelson Chen <crazysim@gmail.com>
2008-08-31 02:01:49 -0700

    commenting

--- generate.rb
+++ generate.rb
@@ -8,4 +8,17 @@ require 'gruff'
 # http://www.imdb.com/title/tt0804503/eprate
 # doc.search("//tr/td[@bgcolor=#eeeeee]").last.parent
 
+# Ask for Link (http://www.imdb.com/title/tt0804503/eprate)
+# Give Image File (tt0804503.png)
+# 
+# Ask for Link
+# Download Page
+# Get elements pertaining to ratings
+# Get Ratings
+# Get Number of Seasons
+# Add the EP's rating to the season total so we can find the average later
+# Create Dataset
+# Generate Graph
+
+
 

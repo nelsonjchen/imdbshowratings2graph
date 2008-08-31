9855fa6a02db12035ef9539cd5b5556d49af6298
Nelson Chen <crazysim@gmail.com>
2008-08-31 01:55:05 -0700

    some initial code and comments

---
+++ generate.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'hpricot'
+require 'gruff'
+
+# Example Link
+# http://www.imdb.com/title/tt0804503/eprate
+# doc.search("//tr/td[@bgcolor=#eeeeee]").last.parent
+
+
